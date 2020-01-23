12cf95c98240fb2a6e30e0541562beef1391862a
ywybzm <31353072+ywybzm@users.noreply.github.com>
2020-01-23 15:56:26 +0800

    Add files via upload

--- 源代码/db_connect.py
+++ 源代码/db_connect.py
@@ -1,5 +1,17 @@
 import pymysql
-def Db_Connect(host,user,passwd,db,port,charset):
-    con = pymysql.connect(host=host, user=user, passwd=passwd, db=db, port=port, charset=charset)
-    cur = con.cursor()
-    return cur
+
+
+class DbConnect:
+
+    def __init__(self, host, user, passwd, db, port, charset):
+        self.con = pymysql.connect(host=host, user=user, passwd=passwd, db=db, port=port, charset=charset)
+        self.cur = self.con.cursor()
+
+    def get_cursor(self):
+        return self.cur
+
+    def close_connection(self):
+        self.cur = None
+        self.con.close()
+
+
